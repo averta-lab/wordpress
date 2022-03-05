92f05c1f8db2bf7b568b0073817e2b353edb5825
Majid <mj_azani@hotmail.com>
2022-03-05 15:10:21 +0330

    default value for get method changed to false

--- src/Cache/DatabaseCache.php
+++ src/Cache/DatabaseCache.php
@@ -5,9 +5,9 @@ namespace Averta\WordPress\Cache;
 class DatabaseCache extends WPCache {
 
 	/**
-	 * @inheritDoc
+	 * {@inheritDoc}
 	 */
-	public function get( $key, $default = null ) {
+	public function get( $key, $default = false ) {
 		global $_wp_using_ext_object_cache;
 
 		$current_using_cache = $_wp_using_ext_object_cache;
@@ -21,7 +21,7 @@ class DatabaseCache extends WPCache {
 	}
 
 	/**
-	 * @inheritDoc
+	 * {@inheritDoc}
 	 */
 	public function set( $key, $value, $ttl = null ): bool {
 		global $_wp_using_ext_object_cache;
@@ -29,7 +29,7 @@ class DatabaseCache extends WPCache {
 		$current_using_cache = $_wp_using_ext_object_cache;
 		$_wp_using_ext_object_cache = false;
 
-		$result = parent::set( $key, $value, $expiration );
+		$result = parent::set( $key, $value, $ttl );
 
 		$_wp_using_ext_object_cache = $current_using_cache;
 
@@ -37,7 +37,7 @@ class DatabaseCache extends WPCache {
 	}
 
 	/**
-	 * @inheritDoc
+	 * {@inheritDoc}
 	 */
 	public function delete( $key ): bool {
 		global $_wp_using_ext_object_cache;
